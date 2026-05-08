d29269064801b3e58dc703178b2af018eb703810
vedant130699 <vedantkulkarni1306@gmail.com>
2026-05-08 22:01:13 +0530

    added dto to secure entities and refactored the code

--- watchlist/src/main/java/com/animewatchlist/watchlist/repository/AnimeRepository.java
+++ watchlist/src/main/java/com/animewatchlist/watchlist/repository/AnimeRepository.java
@@ -6,11 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface AnimeRepository extends JpaRepository<Anime, Integer>{
-    Optional<Object> findAllById(int userId);
 
     List<Anime> findByUserId(int userId);
 
